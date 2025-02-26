6dd70723a1b7a183ea92be83fd650203851ec78b
CioF <coder.walidhasan@gmail.com>
2025-02-26 09:20:56 +0600

    db table create

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
-# User Table (Each user can be in multiple queue positions)
-class User(Base):
+# user table (each user can be in multiple queue positions)
+class User(db.Model):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
@@ -23,26 +24,26 @@ class User(Base):
     password_hash: Mapped[str] = mapped_column(String(256), nullable=False)
     role: Mapped[str] = mapped_column(String(20), default='customer')
 
-    # One user can have multiple queue positions (One-to-Many)
+    # one user can have multiple queue positions (one-to-many)
     queue_positions: Mapped[List["QueuePosition"]] = relationship(
         back_populates="user", cascade="all, delete-orphan"
     )
 
-# Queue Table (Each queue has multiple positions)
-class Queue(Base):
+# queue table (each queue has multiple positions)
+class Queue(db.Model):
     __tablename__ = "queue"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     max_capacity: Mapped[int] = mapped_column(Integer, nullable=False, default=50)
 
-    # One queue can have multiple queue positions (One-to-Many)
+    # one queue can have multiple queue positions (one-to-many)
     positions: Mapped[List["QueuePosition"]] = relationship(
         back_populates= "queue", cascade="all, delete-orphan"
     )
 
-# Queue Position Table (Mapping Users to Queues)
-class QueuePosition(Base):
+# queue position table (mapping users to queues)
+class QueuePosition(db.Model):
     __tablename__ = "queue_position"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
@@ -54,9 +55,15 @@ class QueuePosition(Base):
     status: Mapped[str] = mapped_column(String(20), default='waiting')  # waiting, completed, canceled
     joined_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
-    # Relationship Backlinks
+    # relationship backlinks
     queue: Mapped["Queue"] = relationship(back_populates="positions")
     user: Mapped["User"] = relationship(back_populates="queue_positions")
+
+#create tables
+with app.app_context():
+    db.create_all()
+
+
 
 @app.route("/")
 def home():
